40ebff119f58b3c488f80e69a35fcfb504d3363d
Huda Nassar <hnassar@purdue.edu>
2015-06-17 01:06:44 -0400

    removed duplicates

--- test/MatrixNetwork.jl
+++
@@ -1,7 +0,0 @@
-# create type MatrixNetwork
-type MatrixNetwork
-    n::Int64 # number of columns/rows
-    rp::Vector{Int64} # row pointers
-    ci::Vector{Int64} # column indices
-    vals::Vector{Float64} # corresponding values
-end
