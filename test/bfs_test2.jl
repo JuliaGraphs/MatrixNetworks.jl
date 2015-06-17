809723a7d8a781e6e27f3259939d9fb5a5823837
Huda Nassar <hnassar@purdue.edu>
2015-06-17 11:30:02 -0400

    removed duplicate bfs test

--- test/bfs_test2.jl
+++
@@ -1,8 +0,0 @@
-using MAT
-function bfs_test2()
-    file_path = Pkg.dir("MatrixNetworks/data/bfs_example.mat")
-    file = matopen(file_path)
-    A = read(file,"A")
-    close(file)
-    return bfs(MatrixNetwork(A),1)
-end
