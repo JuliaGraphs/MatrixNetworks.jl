513435d7a99f457bd0ae760ff64787fa69685c7d
Huda Nassar <hnassar@purdue.edu>
2015-06-23 13:05:09 -0400

    added runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+using MatrixNetworks
+using Base.Test
+using Compat
+
+all_tests = ["dfs_test", "bfs_test"]
+
+for t in all_tests
+    test_path = joinpath(Pkg.dir("MatrixNetworks"), "test", "$(t).jl")
+    println("running $(test_path) ...")
+    include(test_path)
+end
