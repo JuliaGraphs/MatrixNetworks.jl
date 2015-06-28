59fe820431055b76cdb0f4bbe6ff3d094ec44311
Huda Nassar <hnassar@purdue.edu>
2015-06-28 19:30:55 -0400

    fixing Lint errors

--- src/readSMAT.jl
+++ src/readSMAT.jl
@@ -1,9 +1,9 @@
 function readSMAT(filename::AbstractString)
     (rows,header) = readdlm(filename;header=true)
     A = sparse(
-               convert(Array{Int64,1},rows[1:header[3],1])+1, 
-               convert(Array{Int64,1},rows[1:header[3],2])+1, 
-               rows[1:header[3],3],
+               convert(Array{Int64,1},rows[:,1])+1, 
+               convert(Array{Int64,1},rows[:,2])+1, 
+               rows[:,3],
                parse(Int,header[1]), 
                parse(Int,header[2])
                )
