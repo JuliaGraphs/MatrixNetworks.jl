20ffa3816006f1932521c090e2e0375d055bd0a1
Huda Nassar <hnassar@purdue.edu>
2015-07-02 12:52:24 -0400

    sample data fix

--- src/load_matrix_network.jl
+++ src/load_matrix_network.jl
@@ -2,15 +2,23 @@ include("readSMAT.jl")
 function load_matrix_network(name::AbstractString)
     basename = joinpath(Pkg.dir("MatrixNetworks"),"data")
     smatfile = joinpath(basename,"$(name).smat")
-    meta_xy = joinpath(basename,"metadata","$(name).xy.smat")
-    meta_labels = joinpath(basename,"metadata","$(name).labels.smat")
+    if isfile(smatfile)
+        return readSMAT(smatfile)
+    else
+        error(@sprintf "The example datafile '%s' does not seem to exist where it should\n" name)
+    end
+end
+
+function load_matrix_network_metadata(name::AbstractString)
+    basename = joinpath(Pkg.dir("MatrixNetworks"),"data")
+    smatfile = joinpath(basename,"$(name).smat")
+    meta_xy = joinpath(basename,"$(name).xy")
+    meta_labels = joinpath(basename,"$(name).labels")
     if isfile(smatfile)
         if isfile(meta_xy) && isfile(meta_labels)
             xy = readdlm(meta_xy)
             labels = readdlm(meta_labels)
             return (readSMAT(smatfile),xy,labels)
-        else
-            return readSMAT(smatfile)
         end
     else
         error(@sprintf "The example datafile '%s' does not seem to exist where it should\n" name)
--- src/matrix_network_datasets.jl
+++ src/matrix_network_datasets.jl
@@ -3,4 +3,7 @@ function matrix_network_datasets()
     content = readdir(datasets_location)
     smat_files = filter(x->contains(x,".smat"),content)
     return smat_files
+    for i = 1:length(smat_files)
+        smat_files[i] = smat_files[i][end-6]
+    end
 end
