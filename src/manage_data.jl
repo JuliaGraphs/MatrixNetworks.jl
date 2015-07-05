30998bc0d8806b91f9dda1fc2760d53d23b99919
Huda Nassar <hnassar@purdue.edu>
2015-07-05 16:07:10 -0400

    lintpragma fix

--- src/manage_data.jl
+++ src/manage_data.jl
@@ -41,7 +41,7 @@ function matrix_network_datasets()
     content = readdir(datasets_location)
     smat_files = filter(x->contains(x,".smat"),content)
     for i = 1:length(smat_files)
-        @lintpragma( "Ignore use of undeclared symbol end" )
+        @lintpragma( "Ignore use of undeclared variable end")
         smat_files[i] = smat_files[i][1:end-5]
     end
     return smat_files
