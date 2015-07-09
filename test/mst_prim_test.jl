28ce0175e2abe63b64e3e334295979ef4d17bbeb
Huda Nassar <hnassar@purdue.edu>
2015-07-09 02:39:53 -0400

    added mst_prim_test

--- test/mst_prim_test.jl
+++ test/mst_prim_test.jl
@@ -7,11 +7,4 @@ function mst_prim_test()
     A = sparse(A)
     T = mst_prim_matrix(A)
     return T
-    
-    A = load_matrix_network("airports")
-A = -A #convert to travel time
-A = max(A,A')
-A = sparse(A)
-(ti,tj,tv) = mst_prim(A)
-
 end
