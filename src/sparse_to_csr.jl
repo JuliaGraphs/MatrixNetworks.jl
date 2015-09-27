57ada590fe3d32c1d6d204dee4bf0bef36848ad7
Huda Nassar <hnassar@purdue.edu>
2015-09-27 02:36:46 -0400

    fixed sparse to csr test error

--- src/sparse_to_csr.jl
+++ src/sparse_to_csr.jl
@@ -16,7 +16,7 @@ Example
 i = [1;2;3]
 j = [3;4;4]
 v = [8;9;10]
-(rp,ci,ai,m) = sparse_to_csr(i,j,v,4)
+(rp,ci,ai,m) = sparse_to_csr(i,j,v)
 ~~~
 """
 
--- test/sparse_to_csr_test.jl
+++ test/sparse_to_csr_test.jl
@@ -2,6 +2,6 @@ function sparse_to_csr_test()
     i = [1;2;3]
     j = [3;4;4]
     v = [8;9;10]
-    (rp,ci,ai,m) = sparse_to_csr(i,j,v,4)
+    (rp,ci,ai,m) = sparse_to_csr(i,j,v)
     return true
 end
