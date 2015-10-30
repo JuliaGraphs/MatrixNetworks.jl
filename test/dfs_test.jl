99be5098cb48c6126e45e0ceea5ccba69753a1be
Huda Nassar <hnassar@purdue.edu>
2015-10-29 21:27:30 -0400

    Add more test cases

--- test/dfs_test.jl
+++ test/dfs_test.jl
@@ -1,5 +1,16 @@
 function dfs_test()
     A = load_matrix_network("dfs_example")
     dfs(MatrixNetwork(A),1)
+    
+    n = 10
+    O = ones(Int64,n-1)
+    Z = zeros(Int64,n)
+    A = Tridiagonal(O,Z,O)
+    B = sparse(full(A))
+    (d,dt,ft,pred) = dfs(B,1)
+    if !(d == collect(0:n-1) & dt == collect(0:n-1) & ft == collect(2*n-1:-1:n) & 
+                                    pred == collect(0:n-1)) 
+        error("dfs test failed")
+    end
     return true
 end
