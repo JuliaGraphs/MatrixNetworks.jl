c1d86f90463912dd0c9908c8eae71429108aa64d
David Gleich <dgleich@purdue.edu>
2016-05-21 18:10:40 -0400

    Added a test case for is_undirected

--- test/matrixnetwork_test.jl
+++ test/matrixnetwork_test.jl
@@ -8,7 +8,14 @@ function matrixnetwork_test()
     
     load_matrix_network_all("minnesota")
     load_matrix_network_all("U3A")
+    
+    #@show issym(sparse([0 1; 0 0]'))
+    @test MatrixNetworks.is_undirected(MatrixNetwork(sparse([0 1; 1 0]))) == true
+    @test MatrixNetworks.is_undirected(MatrixNetwork(sparse([0 1; 0 1]))) == false
+    @test MatrixNetworks.is_undirected(MatrixNetwork(load_matrix_network("dfs_example"))) == false
 
     return true
+    
+    
 end
 
