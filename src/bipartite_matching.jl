b9c93dc415964f35d8c5c28818b76a74d6abc763
Ole Kröger <o.kroeger@opensourc.es>
2019-11-16 12:39:22 +0100

    bit earlier break + bugfix if there is no perfect matching

--- src/bipartite_matching.jl
+++ src/bipartite_matching.jl
@@ -335,75 +335,91 @@ function bipartite_cardinality_matching(ei_in::Vector{Int}, ej_in::Vector{Int}, 
 
         # find augmenting path
         @inbounds while match_len < m
-            pend = 1
-            pstart = 1
+            found = false
+            
             for ei_i in ei
                 # free vertex
                 if matching_ei[ei_i] == 0
+                    pend = 1
+                    pstart = 1
                     process_nodes[pstart] = ei_i
                     depths[pstart] = 1
-                    break
-                end
-            end
 
-            while pstart <= pend
-                node = process_nodes[pstart]
-                depth = depths[pstart]
-                
-                # from ei to ej
-                if depth % 2 == 1
-                    # only works if ei is sorted
-                    for ej_i = index_ei[node]:index_ei[node+1]-1
-                        child_node = ej[ej_i]
-                        # don't use matching edge
-                        if matching_ej[child_node] != node && !used_ej[child_node]
-                            used_ej[child_node] = true
-                            pend += 1
-                            depths[pend] = depth+1
-                            process_nodes[pend] = child_node
-                            parents[pend] = pstart
-                        end
-                    end
-                else # ej to ei (only matching edge)
-                    # if matching edge
-                    match_to = matching_ej[node]
-                    if match_to != 0
-                        if !used_ei[match_to]
-                            used_ei[match_to] = true
-                            pend += 1
-                            depths[pend] = depth+1
-                            process_nodes[pend] = match_to
-                            parents[pend] = pstart
-                        end
-                    else
-                        # found augmenting path
-                        parent = pstart
-                        last = 0
-                        c = 0
-                        while parent != 0
-                            current = process_nodes[parent]
-                            if last != 0 
-                                if c % 2 == 1
-                                    matching_ej[last] = current
-                                    matching_ei[current] = last
+                    # while there are open vertices
+                    while pstart <= pend
+                        node = process_nodes[pstart]
+                        depth = depths[pstart]
+                        
+                        # from ei to ej
+                        if depth % 2 == 1
+                            # only works if ei is sorted
+                            for ej_i = index_ei[node]:index_ei[node+1]-1
+                                child_node = ej[ej_i]
+                                # if connected to free vertex
+                                if matching_ej[child_node] == 0
+                                    # found augmenting path
+                                    parent = pstart
+                                    last = child_node
+                                    c = 0
+                                    while parent != 0
+                                        current = process_nodes[parent]
+                                        if c % 2 == 0
+                                            matching_ej[last] = current
+                                            matching_ei[current] = last
+                                        end
+                                        c += 1
+                                        last = current
+                                        parent = parents[parent]
+                                    end
+                                    # break because we found a path
+                                    found = true
+                                    break
+                                end
+                                
+                                # don't use matching edge
+                                if matching_ej[child_node] != node && !used_ej[child_node]
+                                    used_ej[child_node] = true
+                                    pend += 1
+                                    depths[pend] = depth+1
+                                    process_nodes[pend] = child_node
+                                    parents[pend] = pstart
                                 end
                             end
-                            c += 1
-                            last = current
-                            parent = parents[parent]
+                            found && break
+                        else # ej to ei (only matching edge)
+                            # if matching edge
+                            match_to = matching_ej[node]
+                            # there has to be a matching edge otherwise we would have find an 
+                            # augmenting path
+                            @assert match_to != 0
+                            if !used_ei[match_to]
+                                used_ei[match_to] = true
+                                pend += 1
+                                depths[pend] = depth+1
+                                process_nodes[pend] = match_to
+                                parents[pend] = pstart
+                            end
                         end
-                        # break because we found a path
-                        found = true
-                        break
+                        pstart += 1
                     end
                 end
-                pstart += 1
+                if !found
+                    used_ei .= false
+                    used_ej .= false
+                    process_nodes .= 0
+                    depths        .= 0
+                    parents       .= 0
+                end
+                found && break
             end
             if found
                 match_len += 1
                 if match_len < m
                     used_ei .= false
                     used_ej .= false
+                    process_nodes .= 0
+                    depths        .= 0
+                    parents       .= 0
                 end
                 found = false
             else 
