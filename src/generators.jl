2f3a6099a11892d9db33142d86148fa734f4c1a8
Charlie Colley <ccolley@purdue.edu>
2021-07-23 12:07:53 -0400

    updated Reference information.

--- src/generators.jl
+++ src/generators.jl
@@ -823,24 +823,23 @@ Usage
 A random graph model based off of the evolution of protein-protein interaction
 networks. The method takes an undirected graph and runs a number of steps 
 which randomly selects an existing vertex, duplicates the node, and keeps the
-edges between its neighbor with a given probability. In the original work 
-the authors prove that new vertices will tend to become isolated for 
-p <= 0.567143. 
-
-src: 
-    Large-scale behavior of the partial duplication random graph
-    Felix Hermann & Peter Pfaffelhuber
-    https://arxiv.org/pdf/1408.0904.pdf
+edges between its neighbor with a given probability.
 
 Input
 -----
-- 'A::MatrixNetwork{T}': seed matrix network 
-- `steps::Int': The number of steps to run the procedure
-- `p::Float64': the probability 
+- 'A::MatrixNetwork{T}': an undirected seed MatrixNetwork. 
+- `steps::Int': The number of steps to run the procedure.
+- `p::Float64': the probability to included each edge after duplication.
+
+Reference: 
+    A duplication growth model of gene expression networks 
+    Ashish Bhan, David J Galas, T. Gregory Dewey
+    https://doi.org/10.1093/bioinformatics/18.11.1486
 
 Output
 ------
-- a new matrix network generated through the partial duplication procedure. 
+- A new undirected matrix network generated through the partial duplication 
+  procedure. 
 """ 
 function partial_duplication(A::MatrixNetwork{T},steps::Integer, p::Float64) where T
  
